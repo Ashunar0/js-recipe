d32a0357a3f25838700f456d78eec933e38538db
Ashunar0 <asahi.gaia1530@gmail.com>
2024-08-11 20:12:02 +0900

    ゼミ完了

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,13 +1,30 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const doubleButton = document.getElementById("double-button")
 
 let count = 0
 
 // ボタンをクリックしたときの処理を登録
 plusButton.onclick = function () {
-  // ここにクリック後の処理を書く
   // count を更新
   count += 1
+  // count を表示
+  display.textContent = count
+}
+
+// ボタンをクリックしたときの処理を登録
+minusButton.onclick = function () {
+  // count を更新
+  count -= 1
+  // count を表示
+  display.textContent = count
+}
+
+// ボタンをクリックしたときの処理を登録
+doubleButton.onclick = function () {
+  // count を更新
+  count *= 2
   // count を表示
   display.textContent = count
 }
